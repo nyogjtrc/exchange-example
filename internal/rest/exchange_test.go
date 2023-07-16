5f35c1ed9468db6c46c1a6d28b9490ccb2940176
nyo <nyo4rdace@gmail.com>
2023-07-16 18:21:26 +0800

    add exchange api handler

--- internal/rest/exchange_test.go
+++ internal/rest/exchange_test.go
@@ -10,15 +10,26 @@ import (
 )
 
 func TestExchange(t *testing.T) {
+	gin.SetMode(gin.TestMode)
 	r := gin.Default()
+
+	assert.NoError(t, LoadRateFile("../../rate.json"))
 
 	QueryExchange(r)
 
 	w := httptest.NewRecorder()
-	req, _ := http.NewRequest(http.MethodGet, "?source=USD&target=JPY&amount=$1,525", nil)
+	req, _ := http.NewRequest(http.MethodGet, "/api/exchange?source=USD&target=JPY&amount=$1,525", nil)
 
 	r.ServeHTTP(w, req)
 
 	assert.Equal(t, 200, w.Code)
-	assert.JSONEq(t, `{"msg":"success","amount":"$170.496.53"}`, w.Body.String())
+	assert.JSONEq(t, `{"msg":"success","amount":"$170,496.53"}`, w.Body.String())
+}
+
+func TestLoadRate(t *testing.T) {
+	if assert.NoError(t, LoadRateFile("../../rate.json")) {
+		assert.Equal(t, 1.0, rateMap["TWD"]["TWD"])
+		assert.Equal(t, 1.0, rateMap["JPY"]["JPY"])
+		assert.Equal(t, 1.0, rateMap["USD"]["USD"])
+	}
 }
